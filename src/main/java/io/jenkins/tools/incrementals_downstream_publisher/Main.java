7d4d843396485a0a0f232251683388591d362eb4
Jesse Glick <jglick@cloudbees.com>
2018-04-12 21:05:24 -0400

    Basics here.

--- src/main/java/io/jenkins/tools/incrementals_downstream_publisher/Main.java
+++ src/main/java/io/jenkins/tools/incrementals_downstream_publisher/Main.java
@@ -24,10 +24,38 @@
 
 package io.jenkins.tools.incrementals_downstream_publisher;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+
 public class Main {
 
     public static void main(String[] argv) throws Exception {
-        // TODO
+        String buildURL = System.getenv("BUILD_URL");
+        String jenkinsURL = System.getenv("JENKINS_URL");
+        if (buildURL == null || jenkinsURL == null) {
+            throw new IllegalStateException("Run this inside Jenkins so $BUILD_URL & $JENKINS_URL are set");
+        }
+        DocumentContext data = JsonPath.parse(new URL(buildURL + "api/json?tree=actions[causes[upstreamUrl,upstreamProject,upstreamBuild]]"));
+        String upstreamUrl = data.read("$.actions[0].causes[0].upstreamUrl");
+        System.out.println(upstreamUrl);
+        String upstreamProject = data.read("$.actions[0].causes[0].upstreamProject");
+        System.out.println(upstreamProject);
+        int upstreamBuild = data.read("$.actions[0].causes[0].upstreamBuild");
+        System.out.println(upstreamBuild);
+        data = JsonPath.parse(new URL(jenkinsURL + upstreamUrl + upstreamBuild + "/api/json?tree=actions[revision[hash,pullHash]]"));
+        List<Map<String, String>> hashes = data.read("$.actions[*].revision.['hash', 'pullHash']");
+        String hash = hashes.get(0).entrySet().iterator().next().getValue().substring(0, 12);
+        System.out.println(hash);
+        URL zip = new URL(jenkinsURL + upstreamUrl + upstreamBuild + "/artifact/**/*-rc*." + hash + "/*-rc*." + hash + "*/*zip*/archive.zip");
+        File download = File.createTempFile("download", ".zip");
+        //download.deleteOnExit();
+        FileUtils.copyURLToFile(zip, download);
+        System.out.println(download);
     }
 
 }
