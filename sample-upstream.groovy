60d6156bdc6d95e774f347f7a86caf16e5af6241
Jesse Glick <jglick@cloudbees.com>
2018-04-12 21:59:02 -0400

    Easier way to determine artifacts to archive.

--- sample-upstream.groovy
+++ sample-upstream.groovy
@@ -1,14 +1,12 @@
 node {
   checkout scm
-  sh '''
-if [ -f .mvn/extensions.xml ]
-then
-  mvn -DskipTests -Dset.changelist clean install
-fi
-'''
-  def changelist = sh(returnStdout: true, script: 'echo -n -rc$(git rev-list --first-parent --count HEAD).$(git rev-parse --short=12 HEAD)')
-  dir("$HOME/.m2/repository") {
-    archiveArtifacts "**/*$changelist/*$changelist*"
+  if (fileExists('.mvn/extensions.xml')) {
+    def changelistF = "${pwd tmp: true}/changelist"
+    sh "mvn -DskipTests -Dset.changelist help:evaluate -Dexpression=changelist -Doutput=$changelistF clean install"
+    def changelist = readFile(changelistF)
+    dir("$HOME/.m2/repository") {
+      archiveArtifacts "**/*$changelist/*$changelist*"
+    }
+    build 'downstream'
   }
-  build 'downstream'
 }
