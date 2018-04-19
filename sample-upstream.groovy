ed04a902bc522f3c879a4a93c79d6574a78a68b3
Jesse Glick <jglick@cloudbees.com>
2018-04-19 14:50:00 -0400

    Comment.

--- sample-upstream.groovy
+++ sample-upstream.groovy
@@ -6,6 +6,7 @@ node {
     def changelist = readFile(changelistF)
     dir("$HOME/.m2/repository") {
       archiveArtifacts artifacts: "**/*$changelist/*$changelist*", excludes: '**/*.lastUpdated'
+      // Note: could also add exclusions for any modules configured with -Dmaven.deploy.skip: e.g., `jenkinsci/jenkins/test`.
     }
     build job: 'downstream', parameters: [string(name: 'UPSTREAM_URL', value: BUILD_URL)], quietPeriod: 0
   }
